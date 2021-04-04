4ef069906380f6a1e10556bd4727404e97cac260
duracell27 <volodymyrshmidt@gmail.com>
2021-04-04 23:46:58 +0300

    blog.html ready

--- app/js/main.js
+++ app/js/main.js
@@ -19,6 +19,12 @@ $(function () {
         slidesToShow: 10,
         slidesToScroll: 10
     })
+
+    $('.article-slider__box').slick({
+        arrows: true,
+        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowprev"><img src="images/blog-slide-prev.svg" alt="img"></button>',
+        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrownext"><img src="images/blog-slide-next.svg" alt="img"></button>'
+    })
     
     let mixer = mixitup('.gallery__inner', {
         load: {
